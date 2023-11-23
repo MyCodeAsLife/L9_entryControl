ce045092b3d6b9bc9ebec4ec1ed867dae613cf29
Vladimir <w112vovan@mail.ru>
2023-11-23 14:07:44 +0500

    Добавьте файлы проекта.

---
+++ L9_entryControl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9_entryControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOpen = true;
+            string userInput;
+
+            while (isOpen)
+            {
+                Console.Clear();
+                Console.WriteLine("Чтобы выйти из программы напишите слово - exit");
+                userInput = Console.ReadLine();
+
+                if (userInput == "exit")
+                    isOpen = false;
+            }
+
+        }
+    }
+}
